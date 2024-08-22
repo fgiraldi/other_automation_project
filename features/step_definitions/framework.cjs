cef462523550045e98a9fae477294d5f25948694
Federico Giraldi <fgiraldi@gmail.com>
2024-08-22 15:48:28 -0300

    Other commit

--- features/step_definitions/framework.cjs
+++ features/step_definitions/framework.cjs
@@ -1,1 +1,3 @@
-
+const frameworkSteps = require('../../aqa_framework/src/stepDefinitions/playwright.steps.ts');
+const setCustomWorld = require('../../aqa_framework/support/custom-world.ts');
+const setCommonHooks = require('../../aqa_framework/support/common-hooks.ts');
